968d54d5aec504ab19e39217104d36194927fef1
Marta Haro <143503919+Martharoca@users.noreply.github.com>
2024-03-07 10:47:51 +0000

    last commit

--- src/js/component/secondscounter.jsx
+++ src/js/component/secondscounter.jsx
@@ -6,17 +6,21 @@ function SimpleCounter(props) {
         <div className="watch">
         <i className="far fa-clock"></i>
         </div>
-        <div className="four">{props.digito4 % 10}</div>
+		<div className="four">{props.digito6 % 10}</div>
+        <div className="four">{props.digito5 % 10}</div>
+		<div className="four">{props.digito4 % 10}</div>
         <div className="three">{props.digito3 % 10}</div>
         <div className="two">{props.digito2 % 10}</div>
         <div className="one">{props.digito1 % 10}</div>
     </div>);
 }
 SimpleCounter.propTypes = {
-    digito4: propTypes.array,
-    digito3: propTypes.bool,
-    digito2: propTypes.bool,
-    digito1: propTypes.bool,
+    digito6: propTypes.number,
+    digito5: propTypes.number,
+    digito4: propTypes.number,
+    digito3: propTypes.number,
+	digito2: propTypes.number,
+	digito1: propTypes.number,
 }
 
 export default SimpleCounter
--- src/js/index.js
+++ src/js/index.js
@@ -13,11 +13,14 @@ import SimpleCounter from "../js/component/secondscounter.jsx";
 let counter = 0;
 
 setInterval(function() {
+    const six = Math.floor(counter/100000);
+    const five = Math.floor(counter/10000);
     const four = Math.floor(counter/1000);
     const three = Math.floor(counter/100);
     const two = Math.floor(counter/10);
     const one = Math.floor(counter/1);
-    //console.log(four, three, two, one);
-    counter++;
-    ReactDOM.render(<SimpleCounter digito1={one}digito2={two}digito3={three}digito4={four} />,document.querySelector("#app"));
+    //console.log(one, two, three, four, five, six);
+    
+    ReactDOM.render(<SimpleCounter digito1={one}digito2={two}digito3={three}digito4={four}digito5={five}digito6={six} />,document.querySelector("#app"));
+counter++;
 }, 1000);
